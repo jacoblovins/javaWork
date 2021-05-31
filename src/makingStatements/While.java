35f58bfb3d8e7f4bd910bc3aea9aba5bed576015
jacoblovins <lovins.jacob@yahoo.com>
2021-05-31 08:13:46 -0400

    Do While Loop

--- src/makingStatements/While.java
+++ src/makingStatements/While.java
@@ -8,6 +8,11 @@ public class While {
 			System.out.println("While Countdown: " + num);
 			num -= 10;
 		}
+		
+		do {
+			System.out.println("Do While Countup: " + num);
+			num += 10;
+		} while(num < 10);
 	}
 
 }
