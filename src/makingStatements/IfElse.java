cc29f5aaf0b4adfc27ac42f6b106dd5c330b36df
jacoblovins <lovins.jacob@yahoo.com>
2021-05-31 07:25:56 -0400

    initial commit

---
+++ src/makingStatements/IfElse.java
@@ -0,0 +1,23 @@
+package makingStatements;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		if(5 > 1) System.out.println("Five is greater than one");
+		
+		if(2 < 4) {
+			System.out.println("Two is less than four");
+			System.out.println("Test Succeeded");
+		}
+		
+		int num = 20;
+		
+		if(((num > 5) && (num < 10)) || (num == 12)) {
+			System.out.println("Number is 6-9 inclusive or 12");
+		}
+		
+		
+
+	}
+
+}
