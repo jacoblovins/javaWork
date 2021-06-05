1a2e11eb4af5a45b2587420daf6e88e5175b5cab
jacoblovins <lovins.jacob@yahoo.com>
2021-06-05 10:49:36 -0400

    using scanner to get user input

---
+++ src/makingStatements/userInput.java
@@ -0,0 +1,18 @@
+package makingStatements;
+
+import java.util.Scanner;
+
+public class userInput {
+
+	public static void main(String[] args) {
+		System.out.println("What is your name?");
+	
+		
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello " + name);
+
+	}
+
+} 
