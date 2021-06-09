d19b30fedec9da09e75cc990b3b0e435f16bf001
jacoblovins <lovins.jacob@yahoo.com>
2021-06-09 07:34:50 -0400

    arrayLists practice

--- src/makingStatements/MySweetProgram.java
+++ src/makingStatements/MySweetProgram.java
@@ -1,14 +1,27 @@
 package makingStatements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MySweetProgram {
 
 	public static void main(String[] args) {
-		User user = new User();
+		User me = new User();
+		me.setFirstname("Jacob");
+		me.setLastname("Lovins");
 		
-		user.setFirstname("Sub");
-		user.setLastname("Scriber");
 		
-		System.out.println(user.getFullName());
+		User you = new User();
+		you.setFirstname("Sub");
+		you.setLastname("Scriber");
+		
+		List<User> users = new ArrayList<User>();
+		users.add(you);
+		users.add(me);
+		
+		System.out.println(users.get(1).getFullName());
+		
+		
 	}
 
 }
