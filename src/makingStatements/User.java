9cae13c3fe487df9b3136c43b6e02e0ae2b7ce72
jacoblovins <lovins.jacob@yahoo.com>
2021-06-09 07:07:04 -0400

    looping in the method

--- src/makingStatements/User.java
+++ src/makingStatements/User.java
@@ -5,8 +5,12 @@ public class User {
 	public String firstname;
 	public String lastname;
 	
-	public void output() {
-		System.out.println(firstname + " " + lastname);
+	public void output(int times) {
+		
+		for(int i = 0; i < times; i++) {
+			System.out.println(firstname + " " + lastname);			
+		}
+		
 	}
 	
 }
