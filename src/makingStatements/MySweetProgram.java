0fa87a47cd2c6b2a85ee2b0c8771d775eec19a62
jacoblovins <lovins.jacob@yahoo.com>
2021-06-11 09:23:16 -0400

    constructors

--- src/makingStatements/MySweetProgram.java
+++ src/makingStatements/MySweetProgram.java
@@ -8,10 +8,8 @@ public class MySweetProgram {
 
 	public static void main(String[] args) {
 		
-		Student s = new Student();
-		s.firstname = "Jacob";
-		s.lastname = "Lovins";
-		s.major = "Mustache design";
+		Student s = new Student("Jacob", "Lovins", "Mustache Design");
+		
 		
 		Teacher t = new Teacher(); 
 		t.firstname = "Teach";
--- src/makingStatements/Student.java
+++ src/makingStatements/Student.java
@@ -4,6 +4,16 @@ public class Student extends User {
 	public boolean verified = true;
 	public String major;
 	
+	public Student() {
+	}
+	
+	public Student(String firstname, String lastname, String major) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.major = major;
+		
+	}
+	
 	@Override
 	public void sayHello() {
 		System.out.println("Hi, my major is " + major + ". My name is " + firstname + " " + lastname);
